b36906ff372fc30508a020da1d4c109056fbdc73
aniwange <terteseamos@gmail.com>
2020-02-12 09:29:54 +0100

    feat: first commit

---
+++ src/main/java/com/amos/simple_modular_app/GreetingAppApplication.java
@@ -0,0 +1,29 @@
+package com.amos.simple_modular_app;
+
+import com.example.demo.DemoApplication;
+import com.foreach.across.config.AcrossApplication;
+import com.foreach.across.modules.adminweb.AdminWebModule;
+import com.foreach.across.modules.hibernate.jpa.AcrossHibernateJpaModule;
+import com.foreach.across.modules.web.AcrossWebModule;
+import org.springframework.boot.SpringApplication;
+import org.springframework.context.annotation.Bean;
+
+@AcrossApplication(
+		modules = {
+				AcrossWebModule.NAME,
+				AcrossHibernateJpaModule.NAME,
+				AdminWebModule.NAME,
+				//DemoApplication.NAME
+
+		}
+)
+public class GreetingAppApplication {
+
+	@Bean
+	public DemoApplication getDemo(){
+		return new DemoApplication();
+	}
+	public static void main( String[] args ) {
+		SpringApplication.run( GreetingAppApplication.class, args );
+	}
+}
